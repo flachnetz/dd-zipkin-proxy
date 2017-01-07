a3b5468f4c8919a8fb604787875172aaedb7e79c
Oliver Bestmann <oliver.bestmann@googlemail.com>
2017-01-07 16:44:16 +0100

    Install glide.

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if ! which glide 2> /dev/null ; then
+    echo "Install glide as dependency manager"
+    go get -u github.com/Masterminds/glide/
+fi
+
 # format everything
 go fmt . ./datadog ./example ./zipkin
 
