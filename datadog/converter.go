ebec01e98efe6afb6f6db70108c651e1ae56683b
Oliver Bestmann <obest@esailors.de>
2019-04-08 10:20:52 +0000

    correctly set values in datadog spans.

--- datadog/converter.go
+++ datadog/converter.go
@@ -5,7 +5,9 @@ import (
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"reflect"
 	"time"
+	"unsafe"
 )
 
 func Initialize(addr string) {
@@ -13,7 +15,7 @@ func Initialize(addr string) {
 	tracer.Start(tracer.WithAgentAddr(addr))
 }
 
-func sinkSpan(span *zipkincore.Span) {
+func sinkSpan(span *zipkincore.Span) tracer.Span {
 	startTime := time.Unix(0, span.GetTimestamp()*int64(time.Microsecond))
 	finishTime := startTime.Add(time.Duration(span.GetDuration()) * time.Microsecond)
 
@@ -36,39 +38,33 @@ func sinkSpan(span *zipkincore.Span) {
 		}
 	}
 
-	ddSpan := tracer.StartSpan(span.Name, func(cfg *ddtrace.StartSpanConfig) {
+	var ddSpan ddtrace.Span = tracer.StartSpan(span.Name, func(cfg *ddtrace.StartSpanConfig) {
 		cfg.StartTime = startTime
 		cfg.SpanID = uint64(span.ID)
 		cfg.Tags = tags
-
-		if span.ParentID != nil {
-			cfg.Parent = spanContext{
-				startTime: startTime,
-				traceId:   uint64(span.TraceID),
-				spanId:    uint64(*span.ParentID),
-			}
-		}
 	})
 
+	var context ddtrace.SpanContext = ddSpan.Context()
+
+	refSpan := reflect.ValueOf(ddSpan).Elem()
+	refSpan.FieldByName("TraceID").SetUint(uint64(span.TraceID))
+
+	if span.ParentID != nil {
+		refSpan.FieldByName("ParentID").SetUint(uint64(*span.ParentID))
+	}
+
+	refContext := reflect.ValueOf(context).Elem()
+	setUnexportetFieldValue(refContext.FieldByName("traceID"), uint64(span.TraceID))
+	setUnexportetFieldValue(refContext.FieldByName("spanID"), uint64(span.ID))
+
 	ddSpan.Finish(tracer.FinishTime(finishTime))
+
+	return ddSpan
 }
 
-type spanContext struct {
-	startTime time.Time
-	traceId   uint64
-	spanId    uint64
-}
-
-func (c spanContext) SpanID() uint64 {
-	return c.spanId
-}
-
-func (c spanContext) TraceID() uint64 {
-	return c.traceId
-}
-
-func (c spanContext) ForeachBaggageItem(handler func(k, v string) bool) {
-	return
+func setUnexportetFieldValue(target reflect.Value, value uint64) {
+	ptr := unsafe.Pointer(target.UnsafeAddr())
+	*(*uint64)(ptr) = value
 }
 
 // Reads all zipkin spans from the given channel, converts them to datadog spans using the given converter
