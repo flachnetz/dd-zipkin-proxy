680c733882cab51fea3faa7086b053a85df83251
Oliver Bestmann <obest@esailors.de>
2019-06-16 20:21:25 +0000

    Add a test for the LRU implementation

--- cache/lru_test.go
+++ cache/lru_test.go
@@ -7,9 +7,13 @@ import (
 )
 
 func TestLruCache_Get(t *testing.T) {
+	format := func(idx int) string {
+		return strings.Repeat(fmt.Sprintf("%016d", idx), 16)
+	}
+
 	l := NewLRUCache(1024 * 1024)
 	for idx := 0; idx < 100000; idx++ {
-		l.Set(strings.Repeat(fmt.Sprintf("%016d", idx), 16))
+		l.Set(format(idx))
 	}
 
 	if l.Size() > 2*1024*1024 {
@@ -18,5 +22,11 @@ func TestLruCache_Get(t *testing.T) {
 
 	if l.Count() > 5000 {
 		t.Fatal(l.Size())
+	}
+
+	for idx := 99000; idx < 100000; idx++ {
+		if l.Get(format(idx)) == "" {
+			t.Fatal("Item should be in the cache: " + format(idx))
+		}
 	}
 }
