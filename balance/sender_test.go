7ee9d60dee54f277a8c89d195f48bc4c7fceddc6
Oliver Bestmann <obest@esailors.de>
2019-06-18 10:35:16 +0000

    Add a test for encoding/decoding spans to kafka message

--- balance/sender_test.go
+++ balance/sender_test.go
@@ -1,7 +1,12 @@
 package balance
 
 import (
+	"bytes"
+	"github.com/flachnetz/dd-zipkin-proxy/codec"
 	"github.com/flachnetz/dd-zipkin-proxy/proxy"
+	. "github.com/onsi/gomega"
+	"math/rand"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -31,4 +36,41 @@ func BenchmarkMakeProducerMessage(b *testing.B) {
 	for idx := 0; idx < b.N; idx++ {
 		makeProducerMessage(span, "my topic")
 	}
+}
+
+func TestEncodeDecode(t *testing.T) {
+	var spans []proxy.Span
+	var encoded [][]byte
+
+	for idx := 0; idx < 10000; idx++ {
+		span := proxy.Span{
+			Id:      proxy.Id(rand.Int()),
+			Trace:   proxy.Id(rand.Int()),
+			Parent:  proxy.Id(rand.Int()),
+			Name:    "consume " + strconv.Itoa(rand.Int()),
+			Service: "zipkin-proxy" + strconv.Itoa(rand.Int()),
+
+			Tags: map[string]string{
+				"http.path":       "/some/url",
+				"http.status":     strconv.Itoa(rand.Intn(200) + 200),
+				"protocolVersion": "json v1",
+			},
+
+			Timestamp: proxy.Timestamp(rand.Int63n(1000) + 1560847100*int64(time.Nanosecond)),
+			Duration:  time.Duration(rand.Int63n(1000)) + 50*time.Millisecond,
+
+			Timings: proxy.Timings{
+				CS: proxy.Timestamp(rand.Int63n(1000) + 1560847100*int64(time.Nanosecond)),
+			},
+		}
+
+		spans = append(spans, span)
+		encoded = append(encoded, *(makeProducerMessage(span, "topic").Value.(*ptrSliceEncoder)))
+	}
+
+	g := NewGomegaWithT(t)
+	for idx, span := range spans {
+		g.Expect(codec.BinaryDecode(bytes.NewReader(encoded[idx]))).To(Equal(span))
+	}
+
 }
